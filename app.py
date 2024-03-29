68c117791cc166d114bf543738f988aac7f6814f
Jaimeen <jaimeenlalloo12@gmail.com>
2024-03-29 12:16:05 +0200

    Fifth Commit

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def hello_world():
     return 'Hello World'
  
 # main driver function
-if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+if __name__ == "__main__":
+  logging.info("Flask App Started")
+  flask_app.run(host="0.0.0.0", port=8000)
